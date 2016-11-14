d2c43dba1d2f79b09d05e7d282fcc45f49fd6d4a
legioner147 <salihakgun00@gmail.com>
2016-11-14 23:03:23 +0300

    Günü kapatırkene
    :dancer:

--- StokTakibi/_PresentationLayer/Form1.Designer.cs
+++ StokTakibi/_PresentationLayer/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(473, 361);
+            this.ClientSize = new System.Drawing.Size(380, 288);
             this.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.Name = "Form1";
